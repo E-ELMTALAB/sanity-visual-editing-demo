958daeb271e0567d81dbe5db6838fd524001940b
E-ELMTALAB <117757969+E-ELMTALAB@users.noreply.github.com>
2025-09-28 18:19:38 +0330

    fixing errors , attempt #2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import 'tailwindcss/tailwind.css'
 
 import { IBM_Plex_Mono, Inter, PT_Serif } from 'next/font/google'
-import { VisualEditing } from '@sanity/visual-editing/next/app-router'
+import { VisualEditing } from '@sanity/visual-editing/next'
 
 const serif = PT_Serif({
   variable: '--font-serif',
