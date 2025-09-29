018cef6aecacca67101f8fa1c86be2aef882a880
E-ELMTALAB <117757969+E-ELMTALAB@users.noreply.github.com>
2025-09-29 22:09:05 +0330

    trying to use the sharifgpt website files directly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { theme } = require('@sanity/demo/tailwind')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,6 +9,13 @@ module.exports = {
     './sharifgpt-website/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    ...theme,
+    // Overriding fontFamily to use @next/font loaded families
+    fontFamily: {
+      mono: 'var(--font-mono)',
+      sans: 'var(--font-sans)',
+      serif: 'var(--font-serif)',
+    },
   },
   plugins: [require('@tailwindcss/typography'), require('tailwindcss-animate')],
 }
