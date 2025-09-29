6701aa849f03830d586f5be5e4a73f53e9121be3
E-ELMTALAB <117757969+E-ELMTALAB@users.noreply.github.com>
2025-09-29 21:47:44 +0330

    trying to fix the error , attempt #4

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,9 @@ import { IBM_Plex_Mono, Inter, PT_Serif } from 'next/font/google'
 import dynamic from 'next/dynamic'
 const AppVisualEditing = dynamic(() => import('components/visual-editing/AppVisualEditing'), { ssr: false })
 const AppSharifProviders = dynamic(() => import('components/visual-editing/AppSharifProviders'), { ssr: false })
+// Tailwind v4 pipeline uses the new plugin via postcss
 import '../styles/sharifgpt.css'
+import '../styles/index.css'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
