95fad88a94786d9bc0b9f52c189b453f65ee4c13
E-ELMTALAB <117757969+E-ELMTALAB@users.noreply.github.com>
2025-09-30 16:04:28 +0330

    the visual editing is now working for the hero section , but only for a select few of them more investigation is needed . trying to do the same thing for the promocards as well

--- components/site/home/HeroPromoOverlay.tsx
+++ components/site/home/HeroPromoOverlay.tsx
@@ -5,16 +5,24 @@ interface HeroPromoOverlayProps {
 
 export default function HeroPromoOverlay({ slides, promoCards }: HeroPromoOverlayProps) {
   return (
-    <div className="sr-only">
+    <div aria-hidden="true" style={{ position: 'absolute', inset: 0, pointerEvents: 'none', opacity: 0 }}>
       {slides?.map((s, i) => (
-        <div key={`slide-${i}`}>
+        <div key={`slide-${i}`}
+          data-sanity-id={s?._id || s?._key}
+          data-sanity-type="heroSlide"
+          data-sanity-index={i}
+        >
           <span>{s?.title}</span>
           <span>{s?.subtitle}</span>
           <span>{s?.buttonText}</span>
         </div>
       ))}
       {promoCards?.map((p, i) => (
-        <div key={`promo-${i}`}>
+        <div key={`promo-${i}`}
+          data-sanity-id={p?._id || p?._key}
+          data-sanity-type="promoCard"
+          data-sanity-index={i}
+        >
           <span>{p?.title}</span>
           <span>{p?.subtitle}</span>
           <span>{p?.buttonText}</span>
