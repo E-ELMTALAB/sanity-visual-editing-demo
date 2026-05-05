ccbf6a521090fe155bcc948e73df6247c948cc91
E-ELMTALAB <117757969+E-ELMTALAB@users.noreply.github.com>
2026-05-05 09:22:52 +0000

    Add built dist folder

---
+++ glass-luxe-ui-main/apps/web/dist/assets/label-DvGz3jmg.js
@@ -0,0 +1,1 @@
+import{r,j as t,b6 as a,a as i}from"./vendor-CwRQx74n.js";import{c as l}from"./index-D6augq8h.js";const m=i("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),d=r.forwardRef(({className:s,...e},o)=>t.jsx(a,{ref:o,className:l(m(),s),...e}));d.displayName=a.displayName;export{d as L};
